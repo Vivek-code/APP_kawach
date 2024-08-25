348b9c3ee92777c6aba771eeb5b1bacbec41249a
Vivek <vivekchaurasia2997@gmail.com>
2024-08-25 11:29:38 +0530

    map view

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ dependencies {
     implementation(libs.constraintlayout)
     implementation(libs.play.services.location)
     implementation(libs.firebase.auth)
+    implementation(libs.play.services.maps)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
